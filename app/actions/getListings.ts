8d35fe5d19615abeb0f3aab516f9001b74c751eb
George kimutai <georgekimu@gmail.com>
2023-05-07 18:10:12 +0300

    complete project issue no 4 solved

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,7 +2,7 @@ import { categories } from './../components/navbar/Categories';
 import prisma from '@/app/libs/prismadb'
 
 export interface IListingParams {
-    userId: string
+    userId?: string
     guestCount ?: number
     roomCount ?: number
     bathroomCount ?: number
