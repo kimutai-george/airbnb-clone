d3066b092ff9cc04d187a82bf1d985829d913677
George kimutai <georgekimu@gmail.com>
2023-05-07 17:10:07 +0300

    complete project plus

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
     actionLabel: string
     disabled?:boolean
     secondaryAction?: () => void
-    secondaryActionLabel?: () => void
+    secondaryActionLabel?: string
 
 }
 
